a179f635b8a7f001e83897246756713ee2f354d6
mkh9293 <mkh9293@gmail.com>
2022-05-29 22:12:09 +0900

    fisrt commit

---
+++ src/main/java/io/spring/batch/helloworldbatch/HelloWorldBatchApplication.java
@@ -0,0 +1,90 @@
+package io.spring.batch.helloworldbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class HelloWorldBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldBatchApplication.class, args);
+    }
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step1() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet(helloWorldTasklet(null))
+//                .tasklet(helloWorldTasklet())
+                .build();
+
+//                .tasklet(((stepContribution, chunkContext) -> {
+//                    System.out.println("Hello, world!");
+//                    return RepeatStatus.FINISHED;
+//                })).build();
+
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("Hello, World!");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }).build();
+    }
+
+    // 실제 step(tasklet) 이 호출되기 전, 앱 로딩 시점에 미리 호출된다.
+//    @Bean
+//    public Tasklet helloWorldTasklet() {
+//        System.out.println("test!!!");
+//
+//        return ((stepContribution, chunkContext) -> {
+//           String name = (String) chunkContext.getStepContext()
+//                   .getJobParameters()
+//                   .get("name");
+//
+//           System.out.println(String.format("Hello, %s!", name));
+//           return RepeatStatus.FINISHED;
+//        });
+//    }
+
+    // 늦은 바인딩 사용. 실제 job 이 해당 tasklet 을 호출할 때까지 지연 후 실행된다.
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name
+    ) {
+
+        System.out.println("test!!!");
+        return ((stepContribution, chunkContext) -> {
+            System.out.printf("Hello, %s!%n", name);
+            return RepeatStatus.FINISHED;
+        });
+    }
+
+    @Bean
+    public Job job(){
+        return this.jobBuilderFactory.get("basicJob")
+                .start(step1())
+                .build();
+    }
+
+}
---
+++ src/test/java/io/spring/batch/helloworldbatch/HelloWorldBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.batch.helloworldbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
